ce446ce68688acb05a8fd9068c4b04553638e34c
alexia2212 <alexia_jg@outlook.es>
2023-06-08 18:47:12 -0500

    first commit

---
+++ src/main/java/com/example/videoconferencia/VideoconferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoconferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoconferenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoconferenciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videoconferencia/VideoconferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoconferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoconferenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
